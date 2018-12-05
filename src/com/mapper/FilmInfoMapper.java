8b980a6bbf184f25817eaa7e92605e22c3febf50
wangzehui <wangzehui@vcfilm.cn>
2018-12-05 09:59:06 +0800

    修改

--- src/com/mapper/FilmInfoMapper.java
+++ src/com/mapper/FilmInfoMapper.java
@@ -3,7 +3,10 @@ package com.mapper;
 import java.util.List;
 
 import com.model.FilmInfo;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface FilmInfoMapper {
 
     List<FilmInfo> findFilmInfoByChname(String chname);
